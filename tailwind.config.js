ac0f9f014e689df9f3004ee1de49f3f294276e8d
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-26 00:57:38 +0530

    chore: add tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        modal: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+      },
+      borderWidth: {
+        '3': '3px',
+      },
+      colors: {
+        'light-bg': '#f7fafc',
+        accent: '#805ad5',
+        'accent-hover': '#6b46c1',
+        'selected-bg': '#e2e8f0',
+        'selected-bg-hover': '#cbd5e0',
+        'text-primary': '#4a5568',
+      },
+      fontSize: {
+        xs: '0.75rem',
+      },
+    },
   },
   plugins: [],
 };

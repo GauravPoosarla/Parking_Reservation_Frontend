81d8657b79f20dc764084e2f59724d434026b2a3
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-25 16:04:23 +0530

    feat: add registration and login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-         <h1>Hello, world!</h1>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Login />} />
+          <Route
+            path={'/home'}
+            element={
+              <div>
+                <h1>Home</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+
+export default function Login() {
+  const [haveAccount, sethaveAccount] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const haveAccountHandler = () => {
+    sethaveAccount(!haveAccount);
+  };
+
+  const handleLoginSubmit = event => {
+    event.preventDefault();
+    sendLoginData(email, password);
+  };
+
+  const handleRegisterSubmit = event => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert('Password and confirm password must be the same');
+      return;
+    }
+    sendRegisterData(email, password);
+  };
+
+  const sendLoginData = async (email, password) => {
+    let data = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post('http://localhost:8080/login', data)
+      .then(response => {
+        localStorage.setItem('token', response.data);
+        navigate('/home');
+      })
+      .catch(error => {
+        alert(error.response.data.message);
+      });
+  };
+
+  const sendRegisterData = async (email, password) => {
+    let data = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post('http://localhost:8080/register', data)
+      .then(response => {
+        localStorage.setItem('token', response.data);
+        alert('Register success');
+      })
+      .catch(error => {
+        alert(error.response.data.message);
+      });
+  };
+
+  return (
+    <div className='relative flex flex-col justify-center min-h-screen overflow-hidden'>
+      <div className='w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl'>
+        {haveAccount ? (
+          <h1 className='text-3xl font-semibold text-center text-purple-700 underline'>Sign in</h1>
+        ) : (
+          <h1 className='text-3xl font-semibold text-center text-purple-700 underline'>Sign up</h1>
+        )}
+        <form className='mt-6'>
+          <div className='mb-2'>
+            <label htmlFor='email' className='block text-sm font-semibold text-gray-800'>
+              Email
+            </label>
+            <input
+              type='email'
+              className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='mb-2'>
+            <label htmlFor='password' className='block text-sm font-semibold text-gray-800'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          {!haveAccount && (
+            <div className='mb-2'>
+              <label htmlFor='confirm-password' className='block text-sm font-semibold text-gray-800'>
+                Confirm Password
+              </label>
+              <input
+                type='password'
+                className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                onChange={e => setconfirmPassword(e.target.value)}
+              />
+            </div>
+          )}
+          <a className='text-xs text-purple-600 hover:underline'>Forget Password?</a>
+          <div className='mt-6'>
+            {haveAccount && (
+              <button
+                className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600'
+                onClick={handleLoginSubmit}>
+                Login
+              </button>
+            )}
+            {!haveAccount && (
+              <button
+                className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600'
+                onClick={handleRegisterSubmit}>
+                Sign up
+              </button>
+            )}
+          </div>
+        </form>
+
+        {haveAccount && (
+          <p className='mt-8 text-xs font-light text-center text-gray-700'>
+            {' '}
+            Don&#39;t have an account?{' '}
+            <a className='font-medium text-purple-600 hover:underline cursor-pointer' onClick={haveAccountHandler}>
+              Sign up
+            </a>
+          </p>
+        )}
+        {!haveAccount && (
+          <p className='mt-8 text-xs font-light text-center text-gray-700'>
+            {' '}
+            Already have an account?{' '}
+            <a className='font-medium text-purple-600 hover:underline cursor-pointer' onClick={haveAccountHandler}>
+              Sign in
+            </a>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}

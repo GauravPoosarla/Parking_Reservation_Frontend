17830001f14e3f2f2d4548e0fec902aa436c2ecb
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-28 11:55:11 +0530

    feat: add filter reservations in admin page

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import Navbar from '../../components/Navbar';
+import StyledCalendar from '../../components/StyledCalendar';
 
 const AdminPage = () => {
   const [parkingDetails, setParkingDetails] = useState([]);
+  const [date, setDate] = useState('');
+  const [showCalendar, setShowCalendar] = useState(false);
   useEffect(() => {
     axios
       .get('http://localhost:8000/reservations', {
@@ -17,11 +22,45 @@ const AdminPage = () => {
       });
   }, []);
 
+  const toggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
+  const handleDateReset = () => {
+    setDate('');
+  };
+
+  const filteredParkingDetails = parkingDetails.filter(parking => {
+    if (!date) {
+      return true; // Show all reservations when no date is selected
+    }
+    return parking.date === date.toISOString().split('T')[0];
+  });
+
   return (
     <div>
       <Navbar />
       <div className='container mx-auto mt-8'>
-        <h2 className='text-2xl font-semibold text-purple-700 mb-4'>All Reservations</h2>
+        <div className='flex items-center mb-4 gap-x-2 relative'>
+          <h2 className='text-2xl font-semibold text-purple-700'>All Reservations</h2>
+          <FontAwesomeIcon icon={faFilter} className='text-purple-700 mt-1' onClick={toggleCalendar} />
+          {showCalendar && (
+            <div className='calendar-overlay absolute top-5'>
+              <StyledCalendar
+                onSelectDate={date => {
+                  setDate(date);
+                  toggleCalendar();
+                }}
+              />
+            </div>
+          )}
+          {date && (
+            <div className='flex items-center gap-x-2'>
+              <p className='text-purple-700 text-sm'>Selected date: {date.toISOString().split('T')[0]}</p>
+              <FontAwesomeIcon icon={faTimesCircle} className='text-red-500 cursor-pointer' onClick={handleDateReset} />
+            </div>
+          )}
+        </div>
         <div className='bg-white shadow-md rounded-lg overflow-x-auto'>
           <table className='min-w-full'>
             <thead className='bg-purple-700 text-white'>
@@ -35,7 +74,7 @@ const AdminPage = () => {
               </tr>
             </thead>
             <tbody>
-              {parkingDetails.map(parking => (
+              {filteredParkingDetails.map(parking => (
                 <tr key={parking.id} className='border-b border-gray-200'>
                   <td className='py-4 px-6'>{parking.id}</td>
                   <td className='py-4 px-6'>{parking.startTime}</td>

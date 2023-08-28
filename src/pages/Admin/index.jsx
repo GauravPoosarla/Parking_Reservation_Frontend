f075b624726cf0a2f32c99f57d3b0d450e81be8c
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-28 17:52:01 +0530

    feat: add search functionality for admin

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -9,6 +9,7 @@ const AdminPage = () => {
   const [parkingDetails, setParkingDetails] = useState([]);
   const [date, setDate] = useState('');
   const [showCalendar, setShowCalendar] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
   useEffect(() => {
     axios
       .get('http://localhost:8000/reservations', {
@@ -31,35 +32,50 @@ const AdminPage = () => {
   };
 
   const filteredParkingDetails = parkingDetails.filter(parking => {
-    if (!date) {
-      return true; // Show all reservations when no date is selected
-    }
-    return parking.date === date.toISOString().split('T')[0];
+    const isMatchingDate = !date || parking.date === date.toISOString().split('T')[0];
+    const isMatchingSearch = !searchQuery || parking.userEmail.includes(searchQuery);
+
+    return isMatchingDate && isMatchingSearch;
   });
 
   return (
     <div>
       <Navbar />
       <div className='container mx-auto mt-8'>
-        <div className='flex items-center mb-4 gap-x-2 relative'>
-          <h2 className='text-2xl font-semibold text-purple-700'>All Reservations</h2>
-          <FontAwesomeIcon icon={faFilter} className='text-purple-700 mt-1' onClick={toggleCalendar} />
-          {showCalendar && (
-            <div className='calendar-overlay absolute top-5'>
-              <StyledCalendar
-                onSelectDate={date => {
-                  setDate(date);
-                  toggleCalendar();
-                }}
-              />
-            </div>
-          )}
-          {date && (
-            <div className='flex items-center gap-x-2'>
-              <p className='text-sm font-bold'>Selected date: {date.toISOString().split('T')[0]}</p>
-              <FontAwesomeIcon icon={faTimesCircle} className='text-red-500 cursor-pointer' onClick={handleDateReset} />
-            </div>
-          )}
+        <div className='flex items-center mb-4 gap-x-2 relative justify-between'>
+          <div className='flex items-center gap-x-2'>
+            <h2 className='text-2xl font-semibold text-purple-700'>All Reservations</h2>
+            <FontAwesomeIcon icon={faFilter} className='text-purple-700 mt-1' onClick={toggleCalendar} />
+            {showCalendar && (
+              <div className='calendar-overlay absolute top-5'>
+                <StyledCalendar
+                  onSelectDate={date => {
+                    setDate(date);
+                    toggleCalendar();
+                  }}
+                />
+              </div>
+            )}
+            {date && (
+              <div className='flex items-center gap-x-2'>
+                <p className='text-sm font-bold'>Selected date: {date.toISOString().split('T')[0]}</p>
+                <FontAwesomeIcon
+                  icon={faTimesCircle}
+                  className='text-red-500 cursor-pointer'
+                  onClick={handleDateReset}
+                />
+              </div>
+            )}
+          </div>
+          <div className='flex gap-x-2 items-center'>
+            <input
+              type='text'
+              placeholder='Search by user email...'
+              className='py-2 px-3 border rounded-lg'
+              value={searchQuery}
+              onChange={e => setSearchQuery(e.target.value)}
+            />
+          </div>
         </div>
         <div className='bg-white shadow-md rounded-lg overflow-x-auto'>
           <table className='min-w-full'>

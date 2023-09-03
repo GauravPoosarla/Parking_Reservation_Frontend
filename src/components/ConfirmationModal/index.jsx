9adb17dec6e05e3df94c9dcff7bde0b3887e226e
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-09-03 16:15:18 +0530

    fix: modal content of confirm delete

--- src/components/ConfirmationModal/index.jsx
+++ src/components/ConfirmationModal/index.jsx
@@ -17,7 +17,7 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
               <h3 className='text-lg leading-6 font-medium text-gray-900'>Confirm Deletion</h3>
               <div className='mt-2'>
                 <p className='text-sm text-gray-500'>
-                  Are you sure you want to delete this reservation? This action cannot be undone.
+                  Are you sure you want to cancel this reservation? This action cannot be undone.
                 </p>
               </div>
             </div>
@@ -26,13 +26,13 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
                 type='button'
                 className='inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-500'
                 onClick={onClose}>
-                Cancel
+                Close
               </button>
               <button
                 type='button'
-                className='mr-3 inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500'
+                className='mr-3 inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-700 border border-transparent rounded-md hover:bg-red-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500'
                 onClick={onConfirm}>
-                Delete
+                Cancel
               </button>
             </div>
           </div>
